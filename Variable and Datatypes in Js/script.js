3c63e96569cebc4acee4766789a30b1aa39101fe
gladosAagman <aagman769@gmail.com>
2024-03-19 15:38:32 +0530

    first commit

---
+++ Variable and Datatypes in Js/script.js
@@ -0,0 +1,60 @@
+console.log("welcome to the js")
+
+// var a=4
+// var g=9
+// var c="Aagman"
+
+// console.log(a+b+10)
+// console.log(typeof a, typeof g, typeof c)
+
+console.log("Hey this is tutorial 55");
+
+var a = 5;
+// a = a + 1
+let b = 6;
+let c = "Aagman";
+let _a = "Goldy";
+// var 55a = "Rohan"; // Not Allowed
+
+// console.log(a +  b + 8);
+// console.log(typeof a, typeof b, typeof c);
+{
+    // let a = 66;
+    console.log(a)
+}
+console.log(a)
+// const a1 = 6;
+// a1 = a1 + 1; // Not Allowed because a1 is constant
+
+
+let x = "Aagman bhai";
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+
+let o = {
+    "name": "Aagman",
+    "job code": 5600,
+    "is_handsome": true
+}
+
+console.log(o);
+o.salary = "100crores";
+console.log(o);
+o.salary = "500crores";
+console.log(o);
+
+// This is a JavaScript code that performs various operations related to variables, data types, and objects. Here's a breakdown of what the code does:
+// It starts by defining some variables a, b, and c with values 4, 9, and "Aagman" respectively. It then prints the sum of a and b along with the number 10, and the data types of a, b, and c.
+// It then prints a message "Hey this is tutorial 55".
+// It defines some more variables a, b, c, _a with values 5, 6, "Aagman", and "Goldy" respectively. It also declares a block of code but does not define any variables inside it.
+// It then prints the value of a after the block of code.
+// It defines some variables x, y, z, p, q, r with string, number, float, boolean, undefined, and null values respectively. It then prints the values and their data types.
+// It defines an object o with properties "name", "job code", "is_handsome" and assigns values to them. It then adds a new property "salary" to the object and assigns it a value of "100crores". It repeats this step to change the value of "salary" to "500crores".
+// Overall, this code demonstrates how to declare variables, use different data types, and define objects in JavaScript. It also shows how to modify the values of variables and properties of an object.
