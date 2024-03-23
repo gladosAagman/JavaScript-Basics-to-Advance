35e7d5083b4bdab143df0e8f9065b0c641779a3b
gladosAagman <aagman769@gmail.com>
2024-03-23 12:13:37 +0530

    another commit

--- DOM Parent,children and siblings nodes/index.js
+++ DOM Parent,children and siblings nodes/index.js
@@ -3,6 +3,6 @@ console.log("Hello world")
 document.body
 document.body.childNodes
 
-document.body.firstElementChild 
-document.body.firstElementChild.childNodes 
+document.body.firstElementChild
+document.body.firstElementChild.childNodes
 document.body.firstElementChild.children
